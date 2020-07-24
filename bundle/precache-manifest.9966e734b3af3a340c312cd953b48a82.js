a8ef05ec836f39c4cc45065c92473b54a47b393e
Vladimir16598 <kimvladimir007@gmail.com>
2020-07-24 18:06:59 +0600

    123123123
    123123

---
+++ bundle/precache-manifest.9966e734b3af3a340c312cd953b48a82.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a02d4ba966e53236cba3e814b8b02d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dced451f50c75d8524bb",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "44e3fb9976d972879d3a",
+    "url": "/static/css/main.57027e10.chunk.css"
+  },
+  {
+    "revision": "dced451f50c75d8524bb",
+    "url": "/static/js/2.1b9b2553.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.1b9b2553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44e3fb9976d972879d3a",
+    "url": "/static/js/main.bad6b60e.chunk.js"
+  },
+  {
+    "revision": "d42c9a3e4ded1ce77463",
+    "url": "/static/js/runtime-main.d77ae6d4.js"
+  },
+  {
+    "revision": "e57bce24b152ce8edcf191e44aa575af",
+    "url": "/static/media/Esse.e57bce24.svg"
+  },
+  {
+    "revision": "6ae78314fe9010af1827d3492c21b885",
+    "url": "/static/media/HeartButton.6ae78314.svg"
+  },
+  {
+    "revision": "dc13598fd18f87429c49be51eca2b7bf",
+    "url": "/static/media/catBread.dc13598f.svg"
+  },
+  {
+    "revision": "3bedf8253b10652c8c334f73f8db71ff",
+    "url": "/static/media/catInHat.3bedf825.svg"
+  },
+  {
+    "revision": "39a399e6a9bc92a073c293114acdc3a8",
+    "url": "/static/media/catLiza.39a399e6.svg"
+  }
+]);
